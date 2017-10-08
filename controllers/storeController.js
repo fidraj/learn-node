31ea9a94d3998038ff5b89d5eb344d0340674034
fidraj <fidraj@gmail.com>
2017-10-07 21:52:20 -0400

    Implemented tags list

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -14,7 +14,7 @@ const multerOptions = {
             next({ message: 'That filetype isn\'t allowed.' }, false);
         }
     }
-};
+}
 
 exports.homePage = (req, res) => {
     res.render('index');
@@ -67,8 +67,14 @@ exports.updateStore = async (req, res) => {
     res.redirect(`/stores/${store._id}/edit`);
 }
 
-exports.getStoreBySlug = async(req, res, next) => {
+exports.getStoreBySlug = async (req, res, next) => {
     const store = await Store.findOne({ slug: req.params.slug });
     if(!store) return next();
     res.render('store', { store: store, title: store.name });
+}
+
+exports.getStoresByTag = async (req, res) => {
+    const tags = await Store.getTagsList();
+    const tag = req.params.tag;
+    res.render('tag', { tags, title: 'Tags', tag });
 }
