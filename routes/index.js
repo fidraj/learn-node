79c66ea0e399c77a3e29fe6269e4b3e594b7a298
fidraj <fidraj@gmail.com>
2017-11-06 19:40:15 +0100

    Implemented type ahead

--- routes/index.js
+++ routes/index.js
@@ -43,4 +43,6 @@ router.post('/account/forgot', catchErrors(authController.forgot));
 router.get('/account/reset/:token', catchErrors(authController.reset));
 router.post('/account/reset/:token', authController.confirmedPasswords, catchErrors(authController.update));
 
+router.get('/api/search', catchErrors(storeController.searchStores));
+
 module.exports = router;
