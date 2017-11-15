402caf714f36ceda0f27c8d6df41adbe4276e5d9
fidraj <fidraj@gmail.com>
2017-11-15 21:11:27 +0100

    Implemented Displaying our Hearted Stores

--- routes/index.js
+++ routes/index.js
@@ -43,6 +43,7 @@ router.post('/account/forgot', catchErrors(authController.forgot));
 router.get('/account/reset/:token', catchErrors(authController.reset));
 router.post('/account/reset/:token', authController.confirmedPasswords, catchErrors(authController.update));
 router.get('/map', storeController.mapPage);
+router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
 
 router.get('/api/search', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
